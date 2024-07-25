1a89fc273876622b7ad46d1b25638858d16a4017
Andrew Fox <foxandrew78@gmail.com>
2024-07-25 22:13:53 +0000

    ADD scroll position memory to router. UPDATE content pages with placeholder text. UPDATE theme to use electric blue as tertiary colour. UPDATE icon colours on navbar (themed) + toggle sidebar icon is dynamically made red when it is set to close. ADD code to content-container to assign class based on route (currently commented out)

--- ATAC/src/app/content-container/content-container.component.ts
+++ ATAC/src/app/content-container/content-container.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { SidebarService } from '../shared/services/sidebar.service';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-content-container',
@@ -17,4 +18,25 @@ export class ContentContainerComponent {
   onCloseSideNav = (method: 'esc' | 'backdrop') => {
     this.sideBarService.toggleSidebar(method);
   };
+
+  //? I wrote this code to update the class based on the route so that I could colour the background of the routes without padding etc - it works, but there is still a fex px of padding at the top that I can't figure out just yet!
+  /*
+  router = new Router();
+  currentRoute: string = '';
+  requiredClass = signal('');
+
+  updateBodyClass() {
+    this.requiredClass.set(this.currentRoute.slice(1));
+  }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute = event.urlAfterRedirects;
+        this.updateBodyClass();
+      });
+  }
+      */
+  //! To set the class in the html - add this: [classList]="requiredClass()" (i.e. set the classlist to the signal I created)
 }
