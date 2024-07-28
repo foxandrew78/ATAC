089a94127262ba2c24945968d0d88f8fa3f269ec
Andrew Fox <foxandrew78@gmail.com>
2024-07-28 11:09:15 +0100

    UPDATE added prefix variables at front of all route titles

--- ATAC/src/app/app.routes.ts
+++ ATAC/src/app/app.routes.ts
@@ -1,27 +1,25 @@
 import { Routes } from '@angular/router';
-import { PassdownComponent } from './content/passdown/passdown.component';
-import { ATACComponent } from './content/atac/atac.component';
-import { ARCCComponent } from './content/arcc/arcc.component';
-import { OperationsComponent } from './content/operations/operations.component';
-import { EngineeringComponent } from './content/engineering/engineering.component';
+
+const titlePre = 'SAR.Next - ';
 
 export const routes: Routes = [
   {
     path: 'ATAC',
     loadComponent: () =>
       import('./content/atac/atac.component').then((mod) => mod.ATACComponent),
+    title: titlePre + 'ATAC',
   },
   {
     path: 'ATAC/new',
     loadComponent: () =>
       import('./content/atac/atac.component').then((mod) => mod.ATACComponent),
-    title: 'ATAC',
+    title: titlePre + 'Creat a new ATAC',
   },
   {
     path: 'ARCC',
     loadComponent: () =>
       import('./content/arcc/arcc.component').then((mod) => mod.ARCCComponent),
-    title: 'ARCC',
+    title: titlePre + 'ARCC',
   },
   {
     path: 'passdown',
@@ -29,7 +27,7 @@ export const routes: Routes = [
       import('./content/passdown/passdown.component').then(
         (mod) => mod.PassdownComponent
       ),
-    title: 'Passdown',
+    title: titlePre + 'Passdown',
   },
   {
     path: 'operations',
@@ -37,7 +35,7 @@ export const routes: Routes = [
       import('./content/operations/operations.component').then(
         (mod) => mod.OperationsComponent
       ),
-    title: 'Operations',
+    title: titlePre + 'Operations',
   },
   {
     path: 'engineering',
@@ -45,6 +43,6 @@ export const routes: Routes = [
       import('./content/engineering/engineering.component').then(
         (mod) => mod.EngineeringComponent
       ),
-    title: 'Engineering',
+    title: titlePre + 'Engineering',
   },
 ];
